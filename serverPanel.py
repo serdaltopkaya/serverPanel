9a77eb357db322cde39d0a0df06788cc1089314b
serdal topkaya <serdal.topkaya@gmail.com>
2013-04-28 20:38:34 +0300

    paramiko exec_command() fonc open session just  for one command to run so , im decide to try pexpect modul it can open session for multi command

--- serverPanel.py
+++ serverPanel.py
@@ -1,47 +1,35 @@
 #!/usr/bin/env python
+import pexpect
+import pxssh
 
-import os
-import paramiko
+class SSHConnection(object):
+     def __init__(self):
+          self.user="srdl"
+          self.host="192.168.1.34"
+          self.userpass="ser21"
+          self.rootpass="ser21"
+          self.userClient=pxssh.pxssh()
 
-class sshClass(object):
+     def connection_builder(self):
+          if not self.userClient.login(self.host, self.user,self.userpass):
+               print("connection filed")
+               print(s)
+          else:
+               print ("ssh session login  successfuly")
+               while True:
 
-    def __init__(self,server1, username1, pasword1):   
-        self.server=server1
-        self.username=username1
-        self.password=pasword1
-        self.userClient=paramiko.SSHClient()
-	self.stdin=''
-	self.stdout=''
-	self.stderr=''
-	self.path=''
+                    cmd=raw_input("enter a command to run:   ")
+                    if cmd =="" :
+                         self.userClient.logout()
+                         break
+                    self.userClient.sendline(cmd)
+                    self.userClient.prompt()
+                    print self.userClient.before
+                    self.userClient.sendline("pwd")
+                    self.userClient.prompt()
+                    print "  current directory:   ",  self.userClient.before
 
-    def connection_builder(self):
-        self.userClient.load_host_keys(os.path.expanduser(os.path.join("~", ".ssh", "known_hosts")))
-	
 
-    def test(self):
-	self.connection_builder()
-        try:
-            self.userClient.load_host_keys(os.path.expanduser(os.path.join("~", ".ssh", "known_hosts")))
-            self.userClient.connect(self.server, username=self.username, password=self.password)
-            self.stdin,self.path,self.stderr = self.userClient.exec_command('pwd')
-            self.path=self.path.read()
-            while True:
-                cmd = raw_input("Command to run: ")
-                if cmd == "":
-                    print "its complated";
-	            print "current directory: ",self.path
-		    self.userClient.close()
-	            break
 
-                print "running '%s'" % cmd
-                self.stdin,self.stdout,self.stderr = self.userClient.exec_command(cmd)
-                print self.stdout.read().strip()
-                print "current directory: ",self.path
-
-	finally:
-	    self.userClient.close()        
-
-    
-myclass=sshClass('192.168.1.34','srdl','ser21')
-myclass.test()
+myclass=SSHConnection()
+myclass.connection_builder()
